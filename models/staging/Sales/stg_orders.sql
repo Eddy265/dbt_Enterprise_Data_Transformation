ba4fdb68e2be29ebba44b82126f49000f850c63c
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-03 00:42:04 +0000

    updated stg_orders

--- models/staging/Sales/stg_orders.sql
+++ models/staging/Sales/stg_orders.sql
@@ -1,3 +1,22 @@
-select 
-    *
-from {{ source('Instacart', 'orders') }}
+WITH source AS (
+
+    SELECT * FROM {{ source('Instacart', 'orders') }}
+
+),
+
+renamed as (
+
+    SELECT order_id,
+        customer_id,
+        order_dow,
+        order_hour_of_day,
+        days_since_prior_order,
+        product_id,
+        quantity as order_quantity,
+        order_date,
+        order_status
+    from source
+)
+
+select * from renamed
+
