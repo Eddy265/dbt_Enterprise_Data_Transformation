acd0862998bbe92be60e181742c9f1ef9919c568
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-03 12:16:33 +0000

    model updated

--- target/run/my_new_project/models/staging/Sales/stg_orders.sql
+++ target/run/my_new_project/models/staging/Sales/stg_orders.sql
@@ -2,8 +2,26 @@
   create or replace   view PC_DBT_DB.dbt_EUzoefuna.stg_orders
   
    as (
-    select 
-    *
-from Instacart.public.orders
+    WITH source AS (
+
+    SELECT * FROM Instacart.public.orders
+
+),
+
+renamed as (
+
+    SELECT order_id,
+        customer_id,
+        order_dow,
+        order_hour_of_day,
+        days_since_prior_order,
+        product_id,
+        quantity,
+        order_date,
+        order_status
+    from source
+)
+
+select * from renamed
   );
 
